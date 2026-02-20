a6f65c11bd74aaac613bf70866243db33b52694f
mariem-99 <mariem.mdalla@horizon-university.tn>
2026-02-20 10:40:36 +0100

    ex1.1

---
+++ src/components/Counter.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+function Counter(){
+    const [count,setCount]=useState(0);
+    return (
+        <div>
+            <h2>Count: {count}</h2>
+            <button onClick={()=>setCount(count+1)}>+</button>
+        </div>
+    )
+}
+export default Counter;
